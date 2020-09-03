b83f1353051b0380a0b47aa3efd4f0a19ddfed7a
marx-saul <qqqxxx9@gmail.com>
2020-09-03 18:51:32 +0900

    minor change

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/marx-saul/go_pure
+module go_pure
 
 go 1.15
 
 require (
-	github.com/marx-saul/go-pure v0.0.0-20200903084612-11a5b402a977
+	github.com/marx-saul/go_pure v0.0.0-20200903094310-d3b45acd730d
 	golang.org/x/tools v0.0.0-20200903005429-2364a5e8fdcf
 )
--- go_pure_test.go
+++ go_pure_test.go
@@ -3,13 +3,13 @@ package go_pure_test
 import (
 	"testing"
 
-	"github.com/marx-saul/go-pure"
+	"github.com/marx-saul/go_pure"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 // TestAnalyzer is a test for Analyzer.
 func TestAnalyzer(t *testing.T) {
 	testdata := analysistest.TestData()
-	analysistest.Run(t, testdata, pure.Analyzer, "a")
+	analysistest.Run(t, testdata, go_pure.Analyzer, "a")
 }
 
